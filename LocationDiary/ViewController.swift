1ce88f5aeda601fdf138ea113e60a10f2042b5be
1002541 <hansung@sk.com>
2016-02-18 19:11:50 +0900

    Initial Commit

---
+++ LocationDiary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocationDiary
+//
+//  Created by 1002541 on 2016. 2. 18..
+//  Copyright © 2016년 1002541. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
